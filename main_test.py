f11f9c698d449d17dc3223be4955e12f3dd29eff
Dave Smith <davewsmith@gmail.com>
2017-02-22 15:01:04 -0800

    Add a few starter tests

--- main_test.py
+++ main_test.py
@@ -1,4 +1,10 @@
+# A rough starting point for testing
+#
 # See http://code.google.com/appengine/docs/python/tools/localunittesting.html
+#
+# Note that webest, by virtue of bypassing dev_appserver, doesn't trigger
+# the bevahior required by 'login: (required|admin)' in app.yaml
+
 
 import sys
 import unittest
@@ -11,19 +17,39 @@ import main
 import webtest
 
 from google.appengine.ext import testbed
+from google.appengine.api import users
 
-class ExampleTest(unittest.TestCase):
+
+class TestBase(unittest.TestCase):
 
     def setUp(self):
-        self.testbed = testbed.TestBed()
+        self.testbed = testbed.Testbed()
         self.testbed.activate()
+        self.testbed.init_user_stub()
         self.requester = webtest.TestApp(main.application)
 
     def tearDown(self):
         self.testbed.deactivate()
 
-    def login(self, email, is_admin):
-        self.testbed.set_env(user_email=email, overwrite=True)
-        self.testbed.set_env(user_id=email, overwrite=True)
-        self.testbed.set_env(user_is_admin='1' if is_admin else '0', overwrite=True)
+    def login(self, email, is_admin=False):
+        self.testbed.setup_env(user_email=email, overwrite=True)
+        self.testbed.setup_env(user_id=email, overwrite=True)
+        self.testbed.setup_env(user_is_admin=('1' if is_admin else '0'), overwrite=True)
+
+
+class MainTest(TestBase):
+
+    def test_not_logged_in(self):
+        response = self.requester.get('/')
+        self.assertEquals(200, response.status_code)
+        self.assertIn("Hello", response.body)
+        self.assertNotIn("@", response.body)
+        self.assertIn("login", response.body)
+
+    def test_logged_in(self):
+        self.login("user@example.com")
+        response = self.requester.get('/')
+        self.assertEquals(200, response.status_code)
+        self.assertIn("user@example.com", response.body)
+        self.assertIn("logout", response.body)
 
